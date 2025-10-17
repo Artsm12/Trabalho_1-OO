19c114b705f86f9741bdf5a6977db7adbc673ac3
Arthur <16869841624@estudante.ufjf.br>
2025-10-17 12:09:36 -0300

    Add files via upload

---
+++ Jogo/src/main/java/ufjf/dcc025/trabalho/jogo/Singleplayer.java
@@ -0,0 +1,15 @@
+package ufjf.dcc025.trabalho.jogo;
+
+public class Singleplayer {
+    private Tabuleiro tabuleiro;
+    private Personagem[] player;
+    private Personagem[] computador;
+    private int primeiroJogador;
+
+    public Singleplayer(int primeiroJogador) {
+        this.primeiroJogador = primeiroJogador;
+    }
+    
+    public void inicio() {
+    }
+}
