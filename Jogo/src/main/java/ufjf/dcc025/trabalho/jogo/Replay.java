ac5a9d16ee11cdc96123e51002e7d3d5a7eae678
Arthur <16869841624@estudante.ufjf.br>
2025-10-30 00:17:12 -0300

    Codigo atualizado

---
+++ Jogo/src/main/java/ufjf/dcc025/trabalho/jogo/Replay.java
@@ -0,0 +1,13 @@
+package ufjf.dcc025.trabalho.jogo;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Replay {
+    private List<Tabuleiro> tabuleiros = new ArrayList<>();
+    private List<String> jogadas = new ArrayList<>();
+    
+    public Replay() {
+        
+    }
+}
